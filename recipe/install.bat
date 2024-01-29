1a96b78e94ea06acc04a31793002a9ef673ef6b9
Kerry Su <me@sshockwave.net>
2024-01-29 15:41:41 +0800

    Update install.bat

--- recipe/install.bat
+++ recipe/install.bat
@@ -3,9 +3,6 @@ cd build
 cmake --install . --prefix "%LIBRARY_PREFIX%"
 if errorlevel 1 exit 1
 
-del "%LIBRARY_LIB%"\jxl*.lib
-if errorlevel 1 exit 1
-
 if [%PKG_NAME%] == [libjxl] (
   del "%LIBRARY_BIN%\cjxl.exe"
   if errorlevel 1 exit 1
