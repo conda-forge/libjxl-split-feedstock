8a8d1f96e97c6738be108bf7eb5699374a73b86c
Nils DEYBACH <68770774+ndeybach@users.noreply.github.com>
2026-01-09 11:22:57 +0100

    wip: tries to fix macos link/build test
    - the tests scripts failed because of not found dyld paths. Hence we force them here. TBD

--- recipe/test_pkgconfig_link.sh
+++ recipe/test_pkgconfig_link.sh
@@ -8,4 +8,8 @@ EOF
 
 # Use CC if provided, otherwise fall back to cc
 "${CC:-cc}" conftest.c $(pkg-config --cflags --libs libjxl) -o conftest
+
+# Set library path for runtime on macOS (DYLD_LIBRARY_PATH) and Linux (LD_LIBRARY_PATH)
+export DYLD_LIBRARY_PATH="${PREFIX}/lib${DYLD_LIBRARY_PATH:+:$DYLD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${PREFIX}/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 ./conftest
